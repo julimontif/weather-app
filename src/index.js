275c1bff274e32b742b96ff6b4c65fe20afe1dd2
julimontif <julimontif@gmail.com>
2024-06-01 13:09:56 -0400

    Injected image of the icon for the weather

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
   let temperature = response.data.temperature.current;
   let date = new Date(response.data.time * 1000);
   temperatureElement.innerHTML = Math.round(temperature);
@@ -13,6 +14,9 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`
 
 }
 
@@ -21,6 +25,10 @@ function formatDate(date) {
   let minutes = date.getMinutes();
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusrday", "Friday", "Saturday"];
   let day = days[date.getDay()];
+  
+  if (minutes < 10){
+    minutes = `0${minutes}`;
+  }
   return `${day} ${hours}:${minutes}`;
 }
 
